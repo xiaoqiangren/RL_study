9fbd171a3aabed5e2bbc24c3bf2bd5371439409e
chenxc <1187114138@qq.com>
2020-10-27 17:33:15 +0800

    update_test

--- test_virtual/main.py
+++ test_virtual/main.py
@@ -1,52 +1,18 @@
 import os
+
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
-#import gym_environment
-from stable_baselines.sac.policies import FeedForwardPolicy
-from stable_baselines import SAC
 
-from stable_baselines.common.env_checker import check_env, VecCheckNan
-#from stable_baselines.ddpg.policies import MlpPolicy, LnMlpPolicy, CnnPolicy, LnCnnPolicy, FeedForwardPolicy
-#from stable_baselines.common.policies import FeedForwardPolicy, register_policy
-import time
-
-## these are used to customize cnn policies
-from stable_baselines.common.callbacks import BaseCallback
+from stable_baselines import DDPG
+from stable_baselines.ddpg.policies import LnMlpPolicy
+from stable_baselines import results_plotter
 from stable_baselines.bench import Monitor
 from stable_baselines.results_plotter import load_results, ts2xy
-from stable_baselines import results_plotter
-
-#disable tf warnings
-# https://stackoverflow.com/questions/40426502/is-there-a-way-to-suppress-the-messages-tensorflow-prints/40426709
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
-import warnings
-# https://stackoverflow.com/questions/15777951/how-to-suppress-pandas-future-warning
-warnings.simplefilter(action='ignore', category=FutureWarning)
-#warnings.simplefilter(action='ignore', category=Warning)
-import tensorflow as tf
-tf.get_logger().setLevel('INFO')
-tf.autograph.set_verbosity(0)
-import logging
-tf.get_logger().setLevel(logging.ERROR)
+from stable_baselines.common.noise import AdaptiveParamNoiseSpec
+from stable_baselines.common.callbacks import BaseCallback
 
 
-
-SEED = 0
-np.random.seed(SEED)
-
-####  customise policy
-############### policy with dense network
-
-class CustomPolicy(FeedForwardPolicy):
-    def __init__(self, *args, **kwargs):
-        super(CustomPolicy, self).__init__(*args, **kwargs,layers=[700,700,600],
-                                           layer_norm=False,
-                                           feature_extraction="mlp")
-
-##########################
-# customise callbacks
-##########################
 class SaveOnBestTrainingRewardCallback(BaseCallback):
     """
     Callback for saving a model (the check is done every ``check_freq`` steps)
@@ -57,6 +23,7 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
       It must contains the file created by the ``Monitor`` wrapper.
     :param verbose: (int)
     """
+
     def __init__(self, check_freq: int, log_dir: str, verbose=1):
         super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
         self.check_freq = check_freq
@@ -72,68 +39,45 @@ class SaveOnBestTrainingRewardCallback(BaseCallback):
     def _on_step(self) -> bool:
         if self.n_calls % self.check_freq == 0:
 
-          # Retrieve training reward
-          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
-          if len(x) > 0:
-              # Mean training reward over the last 40 episodes
-              mean_reward = np.mean(y[-40:])
-              if self.verbose > 0:
-                print("Num timesteps: {}".format(self.num_timesteps))
-                print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+            # Retrieve training reward
+            x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+            if len(x) > 0:
+                # Mean training reward over the last 100 episodes
+                mean_reward = np.mean(y[-100:])
+                if self.verbose > 0:
+                    print("Num timesteps: {}".format(self.num_timesteps))
+                    print(
+                        "Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward,
+                                                                                                 mean_reward))
 
-              # New best model, you could save the agent here
-              if mean_reward > self.best_mean_reward:
-                  self.best_mean_reward = mean_reward
-                  # Example for saving best model
-                  if self.verbose > 0:
-                    print("Saving new best model to {}".format(self.save_path))
-                  self.model.save(self.save_path)
+                # New best model, you could save the agent here
+                if mean_reward > self.best_mean_reward:
+                    self.best_mean_reward = mean_reward
+                    # Example for saving best model
+                    if self.verbose > 0:
+                        print("Saving new best model to {}".format(self.save_path))
+                    self.model.save(self.save_path)
 
         return True
 
-##########################
-log_dir = "sac_logger/"
+
+# Create log dir
+log_dir = "tmp/"
 os.makedirs(log_dir, exist_ok=True)
 
 # Create and wrap the environment
-env = gym.make('overtaking-v0')
+env = gym.make('LunarLanderContinuous-v2')
 env = Monitor(env, log_dir)
 
-print("the action dimension:{}".format(env.action_space.shape[-1]))
-#print(env.observation_space)
-
-# It will check your custom environment and output additional warnings if needed
-check_env(env)
-#env = VecCheckNan(env, raise_exception=True)
-
-
-# quick check with random actions on the env.
-# Do this whenever a new environment is added on.
-#obs = env.reset()
-#n_steps = 10
-#for _ in range(n_steps):
-# # Random action
-# action = env.action_space.sample()
-# #print(action)
-# obs, reward, done, infos = env.step(action)
-# print("the reward we received:{}".format(reward))
-# print("the action we received:{}".format(done))
-
-
-model = SAC(CustomPolicy, env, gamma=0.9, learning_rate=0.008598549551828587, learning_starts=5, batch_size=64, buffer_size=1000000, train_freq=2,
-            tau=0.01, ent_coef='auto',target_entropy=-100, verbose=1, tensorboard_log="./sac_filter_tensorboard/")
-
-callback = SaveOnBestTrainingRewardCallback(check_freq=40, log_dir=log_dir)
-time_steps = 3e4
-
-
-start_time = time.time()
+# Add some param noise for exploration
+param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.1, desired_action_stddev=0.1)
+# Because we use parameter noise, we should use a MlpPolicy with layer normalization
+model = DDPG(LnMlpPolicy, env, param_noise=param_noise, verbose=0)
+# Create the callback: check every 1000 steps
+callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
+# Train the agent
+time_steps = 1e5
 model.learn(total_timesteps=int(time_steps), callback=callback)
-print("--- %s seconds ---" % (time.time() - start_time))
 
-# save the last model
-model.save("td3_lastmodel")
-
-results_plotter.plot_results(
-    [log_dir], time_steps, results_plotter.X_TIMESTEPS, "DDPG LunarLander")
+results_plotter.plot_results([log_dir], time_steps, results_plotter.X_TIMESTEPS, "DDPG LunarLander")
 plt.show()
--- test_virtual/test.py
+++ test_virtual/test.py
@@ -1,19 +1,24 @@
 # from stable_baselines import PPO2
+# import highway_env
 # model = PPO2('MlpPolicy','overtaking-vo').learning(1000)
 
 import gym
 import highway_env
-from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.policies import MlpPolicy, FeedForwardPolicy, register_policy
+# from stable_baselines.deepq import DQNPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
+from stable_baselines import DDPG
+from stable_baselines import DQN
 
 env = gym.make("overtaking-v0")
 env.reset()
 model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=10000)
+# model = DQN(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=1000)
 obs = env.reset()
-for i in range(10000):
+for i in range(100):
     action = env.action_type.actions_indexes["IDLE"]
-   # action, _states = model.predict(obs)
+    # action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
     env.render()
